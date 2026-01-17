c4489af4b9e0a52bd812430edbdc73df214337bc
rollboy-tz <rollboytz1@gmail.com>
2026-01-17 03:23:37 +0300

    M

--- src/app.js
+++ src/app.js
@@ -122,7 +122,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/test', testRoutes);
 
 // School Routes
-app.use('/api/schools', schoolRoutes);
+//app.use('/api/schools', schoolRoutes);
 
 // =============================================
 // 4. ERROR HANDLING
