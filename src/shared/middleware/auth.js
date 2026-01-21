560985893c04474d9f21b3b780b573ee9349a1f0
rollboy-tz <rollboytz1@gmail.com>
2026-01-21 19:22:27 +0300

    Import error fixed

--- src/shared/middleware/auth.js
+++ src/shared/middleware/auth.js
@@ -1,8 +1,7 @@
 // middleware/auth.js
 import jwt from 'jsonwebtoken';
-import { pool } from '.../config/database.js';
 import { authLogger, errorLogger } from '../utils/logger.js';
-
+import { pool } from '../../config/database.js';
 export const authenticate = async (req, res, next) => {
   try {
     const authHeader = req.headers.authorization;
